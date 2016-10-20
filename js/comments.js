55332f0efa2cb77eb12ef89e63be8baa9052900d
Marco Beltempo <marco.beltempo@gmail.com>
2016-10-20 00:29:37 -0400

    Changes to comments

--- js/comments.js
+++ js/comments.js
@@ -10,7 +10,7 @@ this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL va
 this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
 */
-(function() { // DON'T EDIT BELOW THIS LINE
+(function comments() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
 s.src = '//beltempomar-co.disqus.com/embed.js';
 s.setAttribute('data-timestamp', +new Date());
