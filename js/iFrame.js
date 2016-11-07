37cca2e9306685b8ad26e16767bc79c5e4e6dbdb
marcob13 <marco.beltempo@gmail.com>
2016-11-07 05:23:18 -0500

    Contact Video

--- js/iFrame.js
+++ js/iFrame.js
@@ -4,16 +4,15 @@
     $(".openpop").click(function (e) {
         e.preventDefault();
         $("iframe").attr("src", $(this).attr('href'));
-        $(".links").fadeOut('slow');
         $(".popup").fadeIn('slow');
     });
 
     $(".iframeClose").click(function () {
-	
-        $(this).parent().fadeOut("slow");
-        $(".links").fadeIn("slow");
-	
-		
+
+        $(".popup").parent().fadeOut("slow");
+        $("#port").fadeIn("slow");
+
+
     });
 	$(".iframeClose").click(function(e) {
      doSomething();
